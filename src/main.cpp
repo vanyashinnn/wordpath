a6c365a4b88b6b036664db6394f939948cfca77c
vanyashinnn <vanyashinnn@rvigroup.ru>
2017-02-27 17:48:46 +0400

    Добавлена проверка количества аргументов

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
 int main(int argc, char* argv[]){
-  return 0;
+    if(argc != 3){
+        std::cout << "\033[32mUse: " << argv[0] << " <path/to/file/with/words> <path/to/file/with/dictionary>\033[0m\n";
+        return 1;
+    }
+    return 0;
 }
