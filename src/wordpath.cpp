47fff340314289af15d3ab52720ca55ba07ffc89
vanyashinnn <vanyashinnn@rvigroup.ru>
2017-02-27 17:42:19 +0400

    Добавил недостающий код

--- src/wordpath.cpp
+++ src/wordpath.cpp
@@ -2,13 +2,15 @@
 
 class WordPath::Impl{
 public:
-    explicit Impl(const std::string& first, const std::string& second, const std::string& wordsFilename){
+    explicit Impl(const std::string& first, const std::string& second, const std::string& wordsFilename):
+    status(WordPath::PATH_NOT_FOUND){
         std::cout << "WordPath::Impl()\n";
     }
     ~Impl(){
         std::cout << "WordPath::~Impl()\n";
     }
     StringList words;
+    WordPath::Error status;
 };
 
 WordPath::WordPath(const std::string& first, const std::string& second, const std::string& wordsFilename):
@@ -22,6 +24,11 @@ WordPath::~WordPath()
     delete pimpl;
     pimpl = 0;
     std::cout << "~WordPath()\n";
+}
+
+WordPath::Error WordPath::status() const
+{
+    return pimpl->status;
 }
 
 StringList WordPath::words() const
--- src/wordpath.h
+++ src/wordpath.h
@@ -6,8 +6,13 @@ typedef std::vector<std::string> StringList;
 class WordPath
 {
 public:
+    enum Error{
+        PATH_FOUND = 0,
+        PATH_NOT_FOUND
+    };
     explicit WordPath(const std::string & first, const std::string & second, const std::string & wordsFilename = "");
     ~WordPath();
+    Error status() const;
     StringList words() const;
 private:
     WordPath(const WordPath&);
