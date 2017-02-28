63dd1e7fe2e0a19dd2a0aa774014c8fea7d32ef8
vanyashinnn <vanyashinnn@rvigroup.ru>
2017-02-28 08:18:23 +0400

    Добавил тесты

--- tests/wordpath.cpp
+++ tests/wordpath.cpp
@@ -3,18 +3,37 @@
 #include "wordpath.h"
 
 TEST(someTest, TestWordPath){
-  std::vector<std::string> stringlist;
-  std::string first("кот");
-  std::string second("сыр");
-  WordPath wordPath(first, second);
-  stringlist = wordPath.words();
+    std::vector<std::string> stringlist;
+    std::string first("кот");
+    std::string second("сыр");
+    WordPath wordPath(first, second);
+    stringlist = wordPath.words();
 
-  WordPath::Error error = wordPath.status();
+    WordPath::Error error = wordPath.status();
 
-  if(stringlist.empty()){
-    EXPECT_EQ(error, WordPath::PATH_NOT_FOUND);
-    return;
-  }
-  EXPECT_EQ(first, stringlist.front());
-  EXPECT_EQ(second, stringlist.back());
+    switch(error){
+        case WordPath::PATH_NOT_FOUND:
+            EXPECT_EQ(stringlist.empty(), true);
+            EXPECT_EQ((stringlist.size() < 2), false);
+            break;
+        case WordPath::PATH_FOUND:
+            EXPECT_EQ(stringlist.empty(), false);
+            EXPECT_EQ(first, stringlist.front());
+            EXPECT_EQ(second, stringlist.back());
+            break;
+        default:
+            EXPECT_TRUE(false);//Сюда мы попасть не должны
+    }
+}
+
+TEST(someTest, OneWord){
+    std::vector<std::string> stringlist;
+    std::string first("кот");
+    WordPath wordPath(first, first);
+    stringlist = wordPath.words();
+
+    EXPECT_EQ(wordPath.status(), WordPath::PATH_FOUND);
+    EXPECT_EQ(stringlist.size(), 1);
+    EXPECT_EQ(first, stringlist.front());
+    EXPECT_EQ(first, stringlist.back());
 }
